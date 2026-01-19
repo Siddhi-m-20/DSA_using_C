0ac36eb795900717931f9c4810948badc8425445
Siddhi Manjarekar <114218813+Siddhi-m-20@users.noreply.github.com>
2026-01-19 22:21:23 +0530

    Modify reverse.c to read user input and reverse name

--- Extra/reverse.c
+++ Extra/reverse.c
@@ -2,14 +2,30 @@
 #include<string.h>
 
 int main(){
-    char name[20]="HelloWorld";
+    char name[20];
+    printf("Enter your name: ");
+    fgets(name, 20, stdin);  // Reads entire line including spaces
+    
+    // Remove the newline character that fgets adds
+    name[strcspn(name, "\n")] = '\0';
+    
     char reverse[20];
-    printf("Name: %s\n",name);
-    int i;
-    for(i=0;i<strlen(name);i++){
-        reverse[strlen(name)-1-i] = name[i];
+    printf("Name: %s\n", name);
+    
+    // Check for empty string
+    if(strlen(name) == 0) {
+        printf("Empty string entered!\n");
+        return 0;
     }
-    reverse[strlen(name)] = '\0'; // Null-terminate the reversed string
-    printf("Reversed Name:%s\n",reverse);
+    
+    int i;
+    int len = strlen(name);
+    for(i=0; i<len; i++){
+        reverse[len-1-i] = name[i];
+    }
+    reverse[len] = '\0';
+    
+    printf("Reversed Name: %s\n", reverse);
     return 0;
 }
+
